27ef3db97c3816d8e7abb599b371c274b934f41d
Sai Charan <boppana.saicharan999@gmail.com>
2025-12-01 22:16:20 +0000

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    [SerializeField] Transform player;
+    private float yaw;
+    private float pitch;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
