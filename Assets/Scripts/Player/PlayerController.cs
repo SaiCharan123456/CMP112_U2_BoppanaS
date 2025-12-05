dc3c43067cb6483b0fe77bcc17941bbfbf2f1f6d
Sai Charan <boppana.saicharan999@gmail.com>
2025-12-05 02:47:21 +0000

    Updated  Player Controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,15 +2,16 @@ using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-
+    [SerializeField] Transform cameraTransform;
     [SerializeField] CharacterController controller;
     private float movementX;
     private float movementY;
     [SerializeField] float speed = 5;
     [SerializeField] float jumpHeight = 1.0f;
     [SerializeField] float gravityValue = -9.81f;
+    [SerializeField] float turningSpeed = 5.0f;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
 
@@ -27,11 +28,29 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
 
+        movement = cameraTransform.transform.TransformDirection(movement);
+        
+        Turn();
+
         controller.Move(movement * speed * Time.deltaTime);
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
 
+        
+
+    }
+
+    void Turn()
+    {
+        Vector3 currentLookDirection = controller.velocity.normalized;
+        currentLookDirection.y = 0;
+
+        currentLookDirection.Normalize();
+
+        Quaternion targetRotation = Quaternion.LookRotation(currentLookDirection);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turningSpeed);
     }
 
     void OnMove(InputValue movementValue)
